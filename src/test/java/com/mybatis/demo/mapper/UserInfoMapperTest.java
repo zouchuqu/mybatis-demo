79bf98615d6c2340c46d4baa2ea39cb29926a8b9
weikechang@hingin.com <hingin201818>
2019-10-11 18:49:28 +0800

    MyBatis详解(1)--MyBatis初体验（基于XMl）

--- src/test/java/com/mybatis/demo/mapper/UserInfoMapperTest.java
+++ src/test/java/com/mybatis/demo/mapper/UserInfoMapperTest.java
@@ -41,15 +41,100 @@ public class UserInfoMapperTest {
         }
     }
 
+
+    //根据id查询user表数据
     @Test
-    public void testSelectAll() {
+    public void testSelectUserInfoById() {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try {
-            //获取
-            List<UserInfo> userInfoList = sqlSession.selectList("selectAll");
-            for (UserInfo userInfo : userInfoList) {
+        /*这个字符串由 userMapper.xml 文件中 两个部分构成
+            <mapper namespace="com.mybatis.demo"> 的 namespace 的值
+            <select id="selectUserById" > id 值*/
+            /*可以单独写 <select id="selectUserById" > id 值*/
+
+//            String statement = "com.mybatis.demo.selectUserInfoById";
+            String statement = "selectUserInfoById";
+            UserInfo userInfo = sqlSession.selectOne(statement, 1);
+            log.info("userInfo={}", userInfo);
+        } finally {
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    //查询所有user表所有数据
+    @Test
+    public void testSelectUserInfoAll() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            String statement = "selectUserInfoAll";
+            List<UserInfo> listUser = sqlSession.selectList(statement);
+            for (UserInfo userInfo : listUser) {
                 log.info("userInfo={}", userInfo);
             }
+        } finally {
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    //模糊查询：根据 user 表的username字段
+    @Test
+    public void testSelectLikeUserName() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            List<UserInfo> listUser = sqlSession.selectList("selectLikeUserName", "%t%");
+            for (UserInfo userInfo : listUser) {
+                log.info("userInfo={}", userInfo);
+            }
+        } finally {
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    //向 user 表中插入一条数据
+    @Test
+    public void testInsertUser() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            UserInfo user = new UserInfo();
+            user.setUsername("Bob");
+            user.setPassword("123456");
+            sqlSession.insert("insertUserInfo", user);
+            //提交插入的数据
+            sqlSession.commit();
+        } finally {
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+    //根据 id 更新 user 表的数据
+    @Test
+    public void testUpdateUserById() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            //如果设置的 id不存在，那么数据库没有数据更改
+            UserInfo user = new UserInfo();
+            user.setId(4L);
+            user.setUsername("Jack");
+            sqlSession.update("updateUserInfoById", user);
+            sqlSession.commit();
+        } finally {
+            //不要忘记关闭 sqlSession
+            sqlSession.close();
+        }
+    }
+
+
+    //根据 id 删除 user 表的数据
+    @Test
+    public void testDeleteUserInfoById() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            sqlSession.delete("deleteUserInfoById", 6);
+            sqlSession.commit();
         } finally {
             //不要忘记关闭 sqlSession
             sqlSession.close();
